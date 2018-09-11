c70f0c9c411353bc803c9beaa079d312ec0adf51
Shuhei YOSHIDA <shuhei.yoshida@jp.oro.com>
2018-09-11 14:05:57 +0900

    クリップボードを cb コマンドで使えるように

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,11 +1,11 @@
 # override
 
-alias ls="ls --color=auto"
-alias grep="grep --color=auto"
-alias tmux="tmux -2 -f \"$XDG_CONFIG_HOME\"/tmux/tmux.conf"
-alias rm="rm -i"
-alias cp="cp -i"
-alias mv="mv -i"
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias tmux="tmux -2 -f $XDG_CONFIG_HOME/tmux/tmux.conf"
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
 
 if [[ -x `which git 2> /dev/null` ]]; then
   alias diff='git diff --no-index'
@@ -21,6 +21,7 @@ fi
 
 # shorten
 
-alias la="ls -lah"
-alias lat="ls -lahtr"
-alias sc="systemctl"
+alias la='ls -lah'
+alias lat='ls -lahtr'
+alias sc='systemctl'
+alias cb='xsel -b'
