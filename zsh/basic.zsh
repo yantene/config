4a920d8f6f4657ed10d852b6389dc134ed436546
Shuhei YOSHIDA <shuhei.yoshida@jp.oro.com>
2019-01-30 14:41:08 +0900

    微修正

--- zsh/basic.zsh
+++ zsh/basic.zsh
@@ -161,7 +161,7 @@ export SKIM_DEFAULT_OPTIONS='
 
 # anyenv
 
-if [[ -d $HOME/.anyenv/bin ]]; then
+if [[ -d $HOME/.anyenv ]]; then
   export PATH="$HOME/.anyenv/bin:$PATH"
   eval "$(anyenv init -)"
 fi
