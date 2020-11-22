83182cc24a319abfef9fc77c30b443eae9fd0257
yantene <contact@yantene.net>
2020-11-23 02:43:27 +0900

    shellcheck tools.zsh

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -1,6 +1,8 @@
+# shellcheck disable=SC2034
+
 alias xpath='xmllint --html --xpath 2> /dev/null'
+# shellcheck disable=SC2032
 alias wunzip='unzip -Ocp932'
-alias trash="mv --backup=numbered --target-directory=$HOME/trash"
 
 cb () {
   if [[ -p /dev/stdin ]] || [[ -f /dev/stdin ]]; then
@@ -23,11 +25,14 @@ alias csv2tsv='ruby -rcsv -ne '\''puts CSV.parse($_)[0].join(%[\t])'\'
 
 # find path
 
-if [[ -x `which sk 2> /dev/null` ]]; then
+if [[ -x $(which sk 2> /dev/null) ]]; then
   function sk-find-path() {
-    local filepath="$( (test -x `which fd 2> /dev/null` && fd -Hc always . || find . 2> /dev/null) | sk)"
+    local filepath
+    # shellcheck disable=SC2015
+    filepath="$( (test -x "$(which fd 2> /dev/null)" && fd -Hc always . || find . 2> /dev/null) | sk)"
     [[ -z "$filepath" ]] && return
-    escaped_filepath=`printf %q "$filepath"`
+    escaped_filepath=$(printf %q "$filepath")
+    # shellcheck disable=SC2153
     if [[ -n "$LBUFFER" ]]; then
       BUFFER="$LBUFFER $escaped_filepath"
     else
@@ -46,9 +51,9 @@ fi
 
 # find line
 
-if [[ -x `which sk 2> /dev/null` ]] && [[ -x `which rg 2> /dev/null` ]]; then
+if [[ -x $(which sk 2> /dev/null) ]] && [[ -x $(which rg 2> /dev/null) ]]; then
   function sk-find-line() {
-    eval $(sk -i -c 'rg --smart-case --line-number --null --color=always "{}"' | cut -d: -f1 | awk -F "\0" "{print \"$EDITOR -c \" \$2 \" \" \"'\"\$1\"'\"}")
+    eval "$(sk -i -c 'rg --smart-case --line-number --null --color=always "{}"' | cut -d: -f1 | awk -F "\0" "{print \"$EDITOR -c \" \$2 \" \" \"'\"\$1\"'\"}")"
   }
 
   zle -N sk-find-line
@@ -59,17 +64,22 @@ fi
 
 alias gup='git rev-parse --is-inside-work-tree > /dev/null 2>&1 && cd `pwd`/`git rev-parse --show-cdup`'
 
-if [[ -x `which git 2> /dev/null` ]]; then
-  g () { [[ $# -eq 0 ]] && git status || git $@ }
+if [[ -x $(which git 2> /dev/null) ]]; then
+  g () {
+    # shellcheck disable=SC2015
+    [[ $# -eq 0 ]] && git status || git "$@"
+  }
   compdef g=git
 fi
 
-if [[ -x `which ghq 2> /dev/null` &&  -x `which sk 2> /dev/null` ]]; then
+if [[ -x $(which ghq 2> /dev/null) &&  -x $(which sk 2> /dev/null) ]]; then
   ghq-cd () {
     if [[ $# -eq 0 ]]; then
-      local repo_path=`sk -c 'ghq list'`
-      [[ $repo_path ]] && cd `ghq root`/$repo_path
+      local repo_path
+      repo_path=$(sk -c 'ghq list')
+      [[ $repo_path ]] && cd "$(ghq root)/$repo_path" || return
     else
+      # shellcheck disable=SC2068
       ghq $@
     fi
   }
@@ -78,15 +88,17 @@ fi
 
 # ssh
 
-if [[ -x `which ssh 2> /dev/null` ]]; then
+if [[ -x $(which ssh 2> /dev/null) ]]; then
   ssh-sk-resetbg () {
     if [[ $# -eq 0 ]]; then
-      local target_host=`sk --ansi -c 'grep "^Host\s\+[^*]*$" ~/.ssh/config.d/*.conf | sed "s/\s\+/\t/g" | cut -f2'`
+      local target_host
+      target_host=$(sk --ansi -c 'grep "^Host\s\+[^*]*$" ~/.ssh/config.d/*.conf | sed "s/\s\+/\t/g" | cut -f2')
       if [[ $target_host ]]; then
-        echo connect to $target_host...
-        ssh $target_host
+        echo "connect to $target_host..."
+        ssh "$target_host"
       fi
     else
+      # shellcheck disable=SC2068
       ssh $@
     fi
 
@@ -100,19 +112,29 @@ fi
 
 # kill
 
-if [[ -x `which sk 2> /dev/null` ]]; then
+if [[ -x $(which sk 2> /dev/null) ]]; then
   function kill-sk () {
     if [[ $# -eq 0 ]]; then
-      local target_process=`sk --multi -c 'ps -ef --no-headers' | awk '{ print $2 }'`
+      local target_process
+      target_process=$(sk --multi -c 'ps -ef --no-headers' | awk '{ print $2 }')
+      # shellcheck disable=SC2086,SC2033
       [[ $target_process ]] && echo $target_process | xargs kill
     else
+      # shellcheck disable=SC2068
       kill $@
     fi
   }
+  # shellcheck disable=SC2032
   alias kill=kill-sk
 fi
 
 # unzipall
 
-function unzipall () { ls -1 $@ | xargs -I{} unzip {} }
-function wunzipall () { ls -1 $@ | xargs -I{} wunzip {} }
+function unzipall () {
+  # shellcheck disable=SC2068
+  find $@ -print0 | xargs -0 -L1 unzip
+}
+function wunzipall () {
+  # shellcheck disable=SC2068,SC2033
+  find $@ -print0 | xargs -0 -L1 wunzip
+}
